229ee15f5d818d3cda8f23e37cf275137a85e677
Arya Sharma <arya.sharma@ibyteinfomatics.in>
2024-12-09 10:40:12 +0530

    swagger api property

--- src/admin/dto/login.dto.ts
+++ src/admin/dto/login.dto.ts
@@ -1,11 +1,14 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class AdminLoginDto {
+  @ApiProperty({ required: true, type: String })
   @IsString()
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ required: true, type: String })
   @IsString()
   @IsNotEmpty()
   password: string;
